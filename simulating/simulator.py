4f516e080992ebc9e4db5226b321d214f46e5a9d
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2023-06-27 05:38:54 -0400

    Now in both places

--- simulating/simulator.py
+++ simulating/simulator.py
@@ -122,7 +122,7 @@ class Simulator:
         self._settings = SimulationSettings(**loaded["simulation_settings"])
         self._state_count = loaded["state_count"]
         self._concept_ids = np.array(loaded["concept_ids"])
-        self._serious_concept_idx = np.array(loaded["serious_concepts"])
+        self._serious_concept_idx = np.array(loaded["serious_concept_idx"])
         self._initial_state_probabilities = np.array(loaded["initial_state_probabilities"])
         self._dynamic_state_entry_coefs = [np.array(matrix) for matrix in loaded["dynamic_state_entry_coefs"]]
         self._dynamic_state_exit_probabilities = np.array(loaded["dynamic_state_exit_probabilities"])
